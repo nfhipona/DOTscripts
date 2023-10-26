ff0431c8a0908821e0d28bb3f34eaf18140edef6
Neil Francis Ramirez Hipona <enef.devops@protonmail.ch>
2023-10-26 14:32:50 +0800

    Integrate alias helper script

--- create_alias.sh
+++ create_alias.sh
@@ -32,17 +32,8 @@ function add_bash_profile_to_zshrc_if_not_exists() {
 #
 # call the function `add_alias_if_not_exists ALIAS` which equate to `echo "$cleandd" >> ~/.bash_profile`
 # ex. add_alias_if_not_exists "$cleandd"
-function add_alias_if_not_exists() {
-    if [ ! -z "$(grep -Fx "$1" ~/.bash_profile)" ]; then
-        # code if found
-        echo "Alias '$1' is already added to bash_profile"
-    else
-        # code if not found
-        echo "Adding alias '$1' to bash_profile..."
-        echo "$1" >> ~/.bash_profile
-        echo "Done."
-    fi
-}
+# ex. sh lib/add_alias_if_not_exists.sh "$cleandd"
+add_alias="lib/add_alias_if_not_exists.sh"
 
 echo ""
 echo "--------------"
@@ -53,15 +44,15 @@ echo "" >> ~/.bash_profile
 
 # xcode --start
 cleandd="alias cleandd=\"$myscripts/xcode/clean_xcode_derivedata.sh\""
-add_alias_if_not_exists "$cleandd"
+sh $add_alias "$cleandd"
 # xcode --end
 
 # git --start
 cbdev="alias cbdev=\"$myscripts/git/checkout_track_dev.sh\""
-add_alias_if_not_exists "$cbdev"
+sh $add_alias "$cbdev"
 
 cbpatch="alias cbpatch=\"$myscripts/git/checkout_track_patch.sh\""
-add_alias_if_not_exists "$cbpatch"
+sh $add_alias "$cbpatch"
 # git --end
 
 # call zshrc function and configure ~/.zshrc
