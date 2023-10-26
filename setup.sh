0a02cdd1e64173090e0c2d059409af49158825cf
Neil Francis Ramirez Hipona <enef.devops@protonmail.ch>
2023-10-26 12:52:20 +0800

    Create setup script

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Type: Setup script
+# Created: October 2023
+
+# equivalent to path: ~/.myscripts
+symlinkdirectory="/Users/$USER/.myscripts"
+myscripts=$(pwd)
+
+# Symbolic Link
+echo ""
+echo "--------------"
+echo ""
+echo "Starting SymLink configuration for '$myscripts'"
+
+#echo "Creating scripts directory at path '$symlinkdirectory'"
+#mkdir -p "$symlinkdirectory"
+
+echo "Linking directory '$myscripts' to temp '$symlinkdirectory'"
+ln -sf $myscripts $symlinkdirectory
+echo ""
+
+if [ ! -z "$(ls -A $symlinkdirectory)" ]; then
+    echo "SymLink successful..."
+    ls -l $symlinkdirectory
+    
+    echo ""
+    echo "Lists of files found on path '$symlinkdirectory':"
+    ls -A $symlinkdirectory
+else
+    echo "SymLink failed..."
+fi
+
+echo ""
+echo "--------------"
+echo ""
