f1d3cafed599141ea70bc17c87cc76e7f052c4fc
Neil Francis Ramirez Hipona <enef.devops@protonmail.ch>
2023-10-26 14:32:12 +0800

    Create helper lib script

---
+++ lib/add_alias_if_not_exists.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Type: Alias setup helper script
+# Created: October 2023
+
+# Usage:
+# define variable to hold the long script
+# cleandd="alias cleandd=\"$myscripts/xcode/clean_xcode_derivedata.sh\""
+#
+# call the function `add_alias_if_not_exists ALIAS` which equate to `echo "$cleandd" >> ~/.bash_profile`
+# ex. add_alias_if_not_exists "$cleandd"
+function add_alias_if_not_exists() {
+    if [ ! -z "$(grep -Fx "$1" ~/.bash_profile)" ]; then
+        # code if found
+        echo "Alias '$1' is already added to bash_profile"
+    else
+        # code if not found
+        echo "Adding alias '$1' to bash_profile..."
+        echo "$1" >> ~/.bash_profile
+        echo "Done."
+    fi
+}
+
+add_alias_if_not_exists "$1"
