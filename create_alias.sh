17c9205c8027b8ace17b6edc333e5f1e5f6353ae
Neil Francis Ramirez Hipona <enef.devops@protonmail.ch>
2023-10-28 10:51:36 +0800

    Update create_alias.sh setup script

--- create_alias.sh
+++ create_alias.sh
@@ -2,44 +2,16 @@
 # Type: Alias setup script
 # Created: October 2023
 
-myscripts=$(echo "~/.myscripts")
-
-# configure ~/.zshrc
-# load ~/.bash_profile
-load_bash_profile="lib/load_bash_profile_to_zshrc.sh"
-
-# Usage:
-# define variable to hold the long script
-# cleandd="alias cleandd=\"$myscripts/xcode/clean_xcode_derivedata.sh\""
-#
-# call the function `add_alias_if_not_exists ALIAS` which equate to `echo "$cleandd" >> ~/.bash_profile`
-# ex. add_alias_if_not_exists "$cleandd"
-# ex. sh lib/add_alias_if_not_exists.sh "$cleandd"
-add_alias="lib/add_alias_if_not_exists.sh"
-
 echo ""
 echo "--------------"
 echo ""
 
-# configure ~/.bash_profile
-echo "" >> ~/.bash_profile
+# run alias_runner script to run and load scripts found in alias/ folder
+sh lib/alias_runner.sh
 
-# xcode --start
-cleandd="alias cleandd=\"$myscripts/xcode/clean_xcode_derivedata.sh\""
-sh $add_alias "$cleandd"
-# xcode --end
-
-# git --start
-cbdev="alias cbdev=\"$myscripts/git/checkout_track_dev.sh\""
-sh $add_alias "$cbdev"
-
-cbpatch="alias cbpatch=\"$myscripts/git/checkout_track_patch.sh\""
-sh $add_alias "$cbpatch"
-# git --end
-
-# call zshrc function and configure ~/.zshrc
+# load ~/.bash_profile to ~/.zshrc
 # remove flag 'true' for ~/.bash_profile source
-sh $load_bash_profile true
+sh lib/load_bash_profile_to_zshrc.sh true
 
 # reload ~/.zshrc
 echo "Reloading ~/.zshrc"
