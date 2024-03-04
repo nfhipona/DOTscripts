dd423b5968aa641ccef7ae23bcaeea9c677a7699
Neil Francis Ramirez Hipona <enef.devops@protonmail.ch>
2024-03-04 09:35:02 +0800

    Add clean xcode archive script

---
+++ xcode/clean_xcode_archives.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Type: Simple xcode script
+# Created: March 2024
+
+# Capture flag set `true` to pause in every run scripts
+flag=$1
+
+folderPath="/Users/$USER/Library/Developer/Xcode/Archives/"
+
+function pause() {
+    if [ ! -z $flag ] && $flag; then
+        echo ""
+        read -s -n 1 -p "🔄 Press any mekus key to continue . . ."
+        echo ""
+    fi;
+}
+
+function purgePath() {
+    # call pause/break
+    pause
+
+    # Proceed on deleting files in the target directory
+    echo ""
+    echo "↳ Purging path '$folderPath' ‼️"
+    rm -rf $folderPath* -y
+
+    showPurgeMessage
+}
+
+function showPurgeMessage() {
+    if [ -z "$(ls $folderPath)" ]; then
+        echo "↳ Purged... ‼️"
+    else
+        echo "↳ Purge failed... ‼️"
+        echo ""
+        echo "📝 Remaining files: ↴"
+        ls $folderPath
+    fi
+}
+
+function startPurge() {
+    echo "⚙️  Staring cleanup for '$USER'"
+
+    # List all files in the directory
+    if [ ! -z "$(ls $folderPath)" ]; then
+        echo ""
+        echo "📝 Lists of files found on path '$folderPath': ↴"
+        ls $folderPath
+
+        # call purge path
+        purgePath
+    else
+        echo ""
+        echo "📝 No files found on path '$folderPath'"
+    fi
+}
+
+echo ""
+echo "--------------"
+echo ""
+
+startPurge
+echo "➤ Done. 🎉"
+
+echo ""
+echo "--------------"
+echo ""
